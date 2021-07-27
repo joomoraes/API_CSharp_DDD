143b1114976f00c662eeb4096202b42923a98359
João Alves <joao.alves@liberali.local>
2021-07-27 17:24:17 -0400

    Etapa de criação da API.Service e API.Data

--- src/Api.Data/Context/ContextFactory.cs
+++ src/Api.Data/Context/ContextFactory.cs
@@ -1,15 +1,17 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 
 namespace Api.Data.Context
 {
-    public class ContextFactory:IDesingTimeCOntextFactory<MyContext>
+    public class ContextFactory:IDesignTimeDbContextFactory<MyContext>
     {
         public MyContext CreateDbContext(string[] args)
         {
-            var connectionString = "Server=localhost;Port=3306;Database=Course;Uid=root;Pwd=admin";
+            var connectionString = "Server=127.0.0.1;Port=3306;Database=dbAPI;Uid=root;Pwd=";
             var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
             optionsBuilder.UseMySql(connectionString);
             return new MyContext(optionsBuilder.Options);
+            
         }
     }
 }
