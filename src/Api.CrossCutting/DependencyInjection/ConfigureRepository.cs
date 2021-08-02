4ad4d15d039139b18a07c207d8844655e68e9019
João Alves <joao.alves@liberali.local>
2021-08-02 15:30:56 -0400

    Desenvolvimento API Ref #31 #29 #30 #27 #28

--- src/Api.CrossCutting/DependencyInjection/ConfigureRepository.cs
+++ src/Api.CrossCutting/DependencyInjection/ConfigureRepository.cs
@@ -1,3 +1,4 @@
+using System;
 using Api.Data.Context;
 using Api.Data.Implementations;
 using Api.Domain.Interfaces;
@@ -15,9 +16,20 @@ namespace Api.CrossCutting.DependencyInjection
 
             serviceColletion.AddScoped<IUserRepository, UserImplementation>();
 
-            serviceColletion.AddDbContext<MyContext> (
-                options => options.UseMySql ("Server=127.0.0.1;Port=3306;Database=dbAPI;Uid=root;Pwd=")
-            );
+            if(Environment.GetEnvironmentVariable("DATABASE").ToLower() == "SQLSERVER".ToLower()) 
+            {
+                serviceColletion.AddDbContext<MyContext> (
+                    //options => options.UseMySql ("Server=127.0.0.1;Port=3306;Database=dbAPI;Uid=root;Pwd=")
+                    options => options.UseSqlServer (Environment.GetEnvironmentVariable("DB_CONNECTION"))
+                );
+            }
+            else 
+            {
+                serviceColletion.AddDbContext<MyContext> (
+                    //options => options.UseMySql ("Server=127.0.0.1;Port=3306;Database=dbAPI;Uid=root;Pwd=")
+                    options => options.UseMySql ("Server=DESKTOP-1DF7BFU;Initial Catalog=APIdb;MultipleActiveResultSets=true;User ID=sa;Password=lbr@2020")
+                );
+            }
         }
     }
 }
